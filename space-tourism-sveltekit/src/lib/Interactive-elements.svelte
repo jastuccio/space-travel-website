615478f1b518f4a78234728b7bfb28e57723cd1e
Joe Astuccio <joe@astucc.io>
2023-01-01 20:00:57 -0500

    Add styles for the underline indicators. Complete lesson 16

--- space-tourism-sveltekit/src/lib/Interactive-elements.svelte
+++ space-tourism-sveltekit/src/lib/Interactive-elements.svelte
@@ -2,20 +2,40 @@
 	<h2 class="numbered-title"><span>03</span> Interactive elements</h2>
 
 	<!-- navigation -->
-	<div />
+	<div>
+		<nav>
+			<ul class="primary-navigation underline-indicators flex" style="gap: 8rem">
+				<li class="active">
+					<a class="uppercase text-white letter-spacing-2" href="#">
+						<span>01</span>
+						Active
+					</a>
+				</li>
+				<li>
+					<a class="uppercase text-white letter-spacing-2" href="#">
+						<span>02</span>
+						Hovered
+					</a>
+				</li>
+				<li>
+					<a class="uppercase text-white letter-spacing-2" href="#"><span>03</span>Idle</a>
+				</li>
+			</ul>
+		</nav>
 
-	<div class="flex">
-		<div style="margin-top: 5rem">
-			<!-- explore button -->
-			<a href="#" class="large-button uppercase ff-serif fs-600 text-dark bg-white">Explore</a>
-		</div>
+		<div class="flex">
+			<div style="margin-top: 5rem">
+				<!-- explore button -->
+				<a href="#" class="large-button uppercase ff-serif fs-600 text-dark bg-white">Explore</a>
+			</div>
 
-		<div style="margin-bottom: 50vh">
-			<!-- Tabs -->
+			<div style="margin-bottom: 50vh">
+				<!-- Tabs -->
 
-			<!-- Dots -->
+				<!-- Dots -->
 
-			<!-- Numbers -->
+				<!-- Numbers -->
+			</div>
 		</div>
 	</div>
 </section>
